c21c696009b9b02ad28fa0d48e22d343bb28cc2f
abmoula <rouah46@gmail.com>
2024-12-21 21:51:57 +0100

    first commit

---
+++ script/flavor_database.js
@@ -0,0 +1,20 @@
+const falvors_list = [
+    {
+        id: "f001",
+        name: "Glace",
+        image: "images/regular.jpg"
+    },
+    {
+        id: "f002",
+        name: "Glace Italienne",
+        image: "images/gelato.jpg"
+    },
+    {
+        id: "f003",
+        name: "Boissons Gelées",
+        image: "images/sorbet.png"
+    },
+   
+]
+
+export default falvors_list;
